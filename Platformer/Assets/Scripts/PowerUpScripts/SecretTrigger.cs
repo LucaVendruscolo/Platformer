581e0af3ceb1d36df8bd4dd104eca4f04769899b
barcysaivaras1 <BarcysA@gmail.com>
2024-12-27 16:50:34 +0000

    Finished Level 3
    + Level 3 now has monsters and powers
    + Adjusted Navmesh Script to follow multiple waypoints
    + Adjust secret Trigger script to now recognise whether something is visible or not and invert it. It now takes an array of objects

--- Platformer/Assets/Scripts/PowerUpScripts/SecretTrigger.cs
+++ Platformer/Assets/Scripts/PowerUpScripts/SecretTrigger.cs
@@ -5,35 +5,28 @@ using UnityEngine;
 public class SecretTrigger : MonoBehaviour
 {
     //Explanation on how to use!
-    //Create a secret part of the level, store it all under 1 game object
-    //Store the game object under the secretLevel gameObject variable
-    //Make the game object invisible
-    //It should work now!
+    // Insert game objects that you want to become visible or invisible into the array
+    // Make them visible in scene if you want them to become invisible when the target is hit
+    // Make them invisible in scene if you want them to become visible when the target is hit
+    // Attach this script to the secret target object
+
+    public GameObject[] secretLevelObjects;
 
 
-    public GameObject secretLevel;
-    private enum secretTypes
-    {
-        ObjectGoesFromInvisibleToVisible,
-        ObjectGoesFromVisibleToInvisible
-    }
-    [SerializeField]
-    private secretTypes secretType;
-
-
-    //Make the level visible when the target has been hit!
+    //Make the level visible or invisble when the target has been hit!
     public void OnTargetHit()
     {
-        //With the use of the enum, we can choose whether we want an object to disappear when we hit the target
-        //OR
-        //We can choose to make it appear when we hit the target
 
-        if (secretTypes.ObjectGoesFromInvisibleToVisible == secretType)
+        for(int i = 0; i < secretLevelObjects.Length; i++)
         {
-            secretLevel.SetActive(true);
+            if (secretLevelObjects[i].activeSelf)
+            {
+                secretLevelObjects[i].SetActive(false);
+            }
+            else if (!secretLevelObjects[i].activeSelf)
+            {
+                secretLevelObjects[i].SetActive(true);
+            }
         }
-        else if (secretTypes.ObjectGoesFromVisibleToInvisible == secretType) { 
-            secretLevel.SetActive(false);
-        }
-    }
+    } 
 }
